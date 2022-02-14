42081fdb0a0d08a69683dd27aafa3a2562c1e8f8
VergaraC <victorvaac@al.insper.edu.br>
2022-02-14 20:29:14 -0300

    l

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+from os import error
+
+arg = sys.argv[1]
+arg =arg.replace(" ","")
+i = 0
+resultado = 0
+nAtual = ""
+listaOrdem = list()
+listaAlgarismos = ["0","1","2","3","4","5","6","7","8","9"]
+print(arg)
+while i< len(arg):
+    print(arg[i])
+    if arg[i].isnumeric():
+        nAtual = nAtual + arg[i]      
+        
+    elif arg[i] == "+" or arg[i] == "-":
+        if nAtual != "":
+            listaOrdem.append(nAtual)
+        listaOrdem.append(arg[i])
+        nAtual = ""
+    else:
+        raise error
+
+    i+=1
+if nAtual != "":
+    listaOrdem.append(nAtual)
+
+print(listaOrdem)
+if listaOrdem[0] == "+" or listaOrdem[0] == "-"  or listaOrdem[len(listaOrdem)-1] == "+" or listaOrdem[len(listaOrdem)-1] == "-":
+    raise error
+j = 1
+resultado = int(listaOrdem[0])
+while j< len(listaOrdem):
+   
+    if listaOrdem[j] == "+":
+        resultado += int(listaOrdem[j+1])
+    elif listaOrdem[j] == "-":
+        resultado -= int(listaOrdem[j+1])
+    j+=1
+print(resultado)

9f24d53fd76402575b18aefefbec24f12baa665c
Týr <VergaraC>
2022-05-25 10:43:49 -0300

    L

--- input.c
+++ input.c
@@ -1,18 +1,15 @@
-
 {
-  /* All str operations */
-  str a, b;
-  int x_1, y, z;
-  x_1 = 1; y = 1; z = 2;
-  a = "abc";
-  b = "def";
-  printf(a.b);
-  printf(a.x_1);
-  printf(x_1.a);
-  printf(y.z);
-  printf(a.(x_1==1));
-  printf(a == a);
-  printf(a < b);
-  printf(a > b);
-
+    int x;
+    x = 3+1;
+    if (x > 1)
+      x = 5-1;
+    if (x == 3) {
+    }
+    else {
+      x = 3;
+    }
+    while (x < 5){
+            x = x+1;
+        }
+    printf(x);
 }
